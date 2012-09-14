538204642ebf0dd21e8029db3f774b104ccb050c
Ross Beale <rossbeale@gmail.com>
2012-09-14 14:17:37 +0100

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TourDroidServer::Application.config.secret_token = '4e22653349240820a6ead63c8a6a20937fe6f7ba0b7c4b078b11364866be833c8cbae584511a151fcd825dc00fd6cb5a0e8e188a2fe74c2830089131ef660040'
