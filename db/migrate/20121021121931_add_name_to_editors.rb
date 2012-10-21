09130fe2c263d51d9b6216b2361d96f9386543db
Ross Beale <rossbeale@gmail.com>
2012-10-21 15:59:25 +0100

    Removing old migrations

--- db/migrate/20121021121931_add_name_to_editors.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToEditors < ActiveRecord::Migration
-  def change
-    add_column :editors, :name, :string
-  end
-end
--- db/migrate/20121021122016_add_super_to_editors.rb
+++
@@ -1,6 +0,0 @@
-class AddSuperToEditors < ActiveRecord::Migration
-  def change
-    add_column :editors, :is_super, :boolean, :default => false
-    add_index :editors, :is_super, :unique => false
-  end
-end
